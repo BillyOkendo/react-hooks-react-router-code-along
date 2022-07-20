b12032daa7e25220de723a29b3af9a07b1c3f950
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-07-20 23:29:51 +0300

    created folder components and added js files to it

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
+import { BrowserRouter, Router } from "react-router-dom";
+import App from "./components/App";
+
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
 
-function App() {
-  return <Home />;
-}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
